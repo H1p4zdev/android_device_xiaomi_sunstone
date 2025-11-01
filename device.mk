3e95732bf74657c0a0c9d76a46fbd3d2825605df
H1p4zdev <138879774+H1p4zdev@users.noreply.github.com>
2025-11-02 04:27:23 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,12 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/xiaomi/sunstone
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -16,17 +17,13 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.holi
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    android.hardware.boot@1.0-service \
     bootctrl.holi \
     libgptutils \
     libz \
     libcutils
 
+# Postinstall / OTA tools
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
